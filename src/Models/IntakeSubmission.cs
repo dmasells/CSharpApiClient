f3c114265d07af5874bc9faefe9ee21b43a04f53
Dylan Masells <dylanmasells@gmail.com>
2021-10-23 07:58:35 -0500

    Add intake submission model
    (cherry picked from commit 1a7dd4be9d5cbadf334f76e5aa1cf21b663d8301)

---
+++ src/Models/IntakeSubmission.cs
@@ -0,0 +1,12 @@
+namespace IntakeQ.ApiClient.Models
+{
+    public class IntakeSubmission
+    {
+        public string IntakeId { get; set; }
+        public string Type { get; set; }
+        public int ClientId { get; set; }
+        public string ExternalPracticeId { get; set; } = "";
+        public string ExternalClientId { get; set; } = "";
+
+    }
+}
