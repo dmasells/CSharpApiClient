1039345a1c6ad1a663492961fb8667d432d32cc7
Dylan Masells <dylanmasells@gmail.com>
2021-10-28 15:08:38 -0500

    Create Models.Webhook namespace; Add TreatmentNoteLocked model

---
+++ src/Models/Webhooks/TreatmentNoteLocked.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntakeQ.ApiClient.Models.Webhooks
+{
+    public class TreatmentNoteLocked
+    {
+        public string NoteId { get; set; }
+        public string Type { get; set; }
+        public int ClientId { get; set; }
+    }
+}
